65b9c03b515f4606c4e61293652a1aa2d401c324
Ana Chiamulera <127249227+chiamulerw@users.noreply.github.com>
2024-07-24 11:25:24 -0300

    Add files via upload

---
+++ node-lib/src/cli.js
@@ -0,0 +1,39 @@
+import chalk from 'chalk';
+import fs from 'fs';
+import pegaArq from './index.js';
+
+const caminho = process.argv;
+
+function imprimeLista(resultados, identificadores = '') {
+  console.log(
+    chalk.yellow('Links em lista'),
+    chalk.black.bgGreen(identificadores),
+    resultados);
+}
+
+
+async function processaTex(argumentos) {
+  const caminho = argumentos[2];
+
+  try {
+    fs.lstatSync(caminho);
+  } catch (erro) {
+    if (erro.code === 'ENOENT') {
+      console.log('Arquivo/diretório não existente');
+      return;
+    }
+  }
+
+  if (fs.lstatSync(caminho).isFile()) {
+    const resultados = await pegaArq(argumentos[2]);
+    imprimeLista(resultados);
+  } else if (fs.lstatSync(caminho).isDirectory()) {
+    const arquivos = await fs.promises.readdir(caminho)
+    arquivos.forEach(async (nomeArquivo) => {
+      const lista = await pegaArq(`${caminho}/${nomeArq}`)
+      imprimeLista(lista, nomeArq)
+    })
+  }
+}
+
+processaTex(caminho);
---
+++ node-lib/src/index.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function extraiLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+  return resultados.length !== 0 ? resultados : 'Sem links no arquivo';
+}
+
+function trataErro(erro) {
+  console.log(erro);
+  throw new Error(chalk.red(erro.code, 'Sem links no diretório'));
+}
+
+// async/await
+
+async function pegaArq(caminhoArq) {
+  try {
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoArq, encoding)
+    return extraiLinks(texto);
+  } catch (erro) {
+    trataErro(erro)
+  }
+}
+
+export default pegaArq;
